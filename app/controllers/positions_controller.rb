641bfdc4e77e07e347757af8d2de7213b1cd6160
sebastiaankosman <sebastiaankosman@gmail.com>
2019-10-26 15:45:12 +0200

    application

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -5,7 +5,7 @@ class PositionsController < ApplicationController
   end
 
   def index
-    @barges = Barge.all
+    @positions = Position.all
   end
 
   def show
@@ -15,9 +15,9 @@ class PositionsController < ApplicationController
   def create
     @position = Position.new(position_params)
     if @position.save!
-      redirect_to @position
 
       PositionMailer.general_message(@position).deliver
+      redirect_to @position
     else
       render :new
     end
