c5abc9309d0aa9586677cf58e39b24ff8dbc600d
sebastiaankosman <sebastiaankosman@gmail.com>
2019-10-27 13:53:32 +0100

    email

--- app/mailers/position_mailer.rb
+++ app/mailers/position_mailer.rb
@@ -4,6 +4,6 @@ class PositionMailer < ActionMailer::Base
 
   def general_message(position)
     @position = position
-    mail(:to => "sebastiaankosman@gmail.com", :subject => "You Have a Message From Your Website")
+    mail(:to => "sebastiaankosman@gmail.com", :subject => "Position update")
   end
 end
