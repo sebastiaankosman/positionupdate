ac1a7d2fa6ddf67194a8314302ef58992325e0f0
sebastiaankosman <sebastiaankosman@gmail.com>
2019-11-03 16:47:36 +0100

    alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,9 @@
 import "bootstrap";
 
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert-demo', {
+  title: "Great",
+  text: "Thank you for the update",
+  icon: "success"
+});
