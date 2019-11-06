d1174fe1f7abf847557ff2e7d70a49157ffef6ee
sebastiaankosman <sebastiaankosman@gmail.com>
2019-11-06 19:32:10 +0100

    render

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -3,13 +3,13 @@ class PositionsController < ApplicationController
     @position = Position.new
   end
 
-  def index
-    @positions = Position.all
-  end
+  # def index
+  #   @positions = Position.all
+  # end
 
-  def show
-    @position = Position.find(params[:id])
-  end
+  # def show
+  #   @position = Position.find(params[:id])
+  # end
 
   def create
     @position = Position.new(position_params)
